5a7f1e51397958b05a6cfe209daa32d8155bd9f9
rodrichulo <rodri100172@gmail.com>
2023-02-14 12:26:41 -0500

    Takes prompts and produces output. Decimals still needed

--- Hotdogs/hotdogs.js
+++ Hotdogs/hotdogs.js
@@ -1,3 +1,27 @@
+numDogs = 0, numFries = 0, numSoda = 0, subtotal = 0;
 numDogs = parseFloat(prompt("hi how many hotdogs would you like", ""));
-total = numDogs * 3.25
-document.write("<br> Your hotdog cost is $" + total);
+numFries = parseFloat(prompt("and how many portion of fries would you like", ""));
+numSoda = parseFloat(prompt("and how many sodas would you like", ""));
+subtotal = (numDogs * 4) + (numFries * 3.5) + (numSoda * 1.75);
+discount = 0;
+
+if (numDogs > 0) document.write("Hotdogs(" + numDogs + "): $" + (numDogs * 4));
+if (numFries > 0) document.write("<br>Fries(" + numFries + "): $" + (numFries * 3.5));
+if (numSoda > 0) document.write("<br>Sodas(" + numSoda + "): $" + (numSoda * 1.75));
+document.write("<br>Subtotal: $" + subtotal);
+
+if (subtotal > 20) {
+    discount = 0.1 * subtotal;
+    discount = (int(discount * 100))/100;
+    console.log(discount);
+    document.write("<br>Discount (For orders over $20): -" + discount);
+    subtotal -= discount;
+    document.write("<br> New Subtotal: $" + (subtotal));
+}
+
+tax = (0.0625 * subtotal);
+tax = (tax * 100)/100;
+document.write("<br> Tax of the subtotal (6.25%) is: $" + tax);
+document.write("<br> final amount is: $" + (subtotal + tax));
+
+
